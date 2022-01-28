a59333b3a9ab3edca7e316fc97a0337227c016a9
Keenan-Kimbrough <keenankimbrough@gmail.com>
2022-01-28 12:45:54 -0500

    debugged and added sonme more javascript

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ const btnNew = document.querySelector('btn-new');
 const btnRoll = document.querySelector('btn-roll');
 const btnHold = document.querySelector('btn-hold');
 
+let currentScore , activePlayer, playing , scores;
 
 
 // Starting conditions
@@ -26,10 +27,10 @@ const btnHold = document.querySelector('btn-hold');
 
 const init = function () {
 
-    const scores = [0, 0]
-    let currentScore = 0;
-    let activePlayer = 0;
-    let playing = true;
+     scores = [0, 0]
+     currentScore = 0;
+     activePlayer = 0;
+     playing = true;
 
     score0El.textContent = 0;
     score1El.textContent = 0;
@@ -122,7 +123,4 @@ scores[activePlayer]
     switchPlayer ();
 }});
 
-btnNew.addEventListener('click', function () {
- 
-
-})
+btnNew.addEventListener('click', init)

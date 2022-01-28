255f3cc919f02bb1b815aedd31fc581b892df597
Keenan-Kimbrough <keenankimbrough@gmail.com>
2022-01-28 12:37:20 -0500

    updated javascript

--- script.js
+++ script.js
@@ -6,8 +6,8 @@
 const player0EL = document.querySelector('.player--0')
 const player1EL = document.querySelector('.player--1')
 
-const score0 = document.querySelector('#score--0');
-const scoreEL = document.getElementById('score--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
 
 const current0El = document.getElemebtById('current--0');
 const current1El = document.getElementById('current--1');
@@ -24,16 +24,26 @@ const btnHold = document.querySelector('btn-hold');
 // Starting conditions
 
 
-scored0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+const init = function () {
 
+    const scores = [0, 0]
+    let currentScore = 0;
+    let activePlayer = 0;
+    let playing = true;
 
-const scores = [0, 0]
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent =0 ;
+    current1EltextContent = 0;
 
+    diceEl.classList.add('hidden');
+    player0EL.classList.remove('player--winner')
+    player0EL.classList.remove('player--winner')
+    player0EL.classList.add('player--active')
+    player0EL.classList.remove('player--active')
+}
+// stats logic from start of application
+init ();
 
 //funtion swith player
 const switchPlayer = function () {
@@ -110,4 +120,9 @@ scores[activePlayer]
     }
     // switch to next player
     switchPlayer ();
-}})
+}});
+
+btnNew.addEventListener('click', function () {
+ 
+
+})
